f02bba03ddd571579ce1a301984fa0a7e50e8d4d
pavani <pavaniavni456@gmail.com>
2022-02-07 15:41:30 +0530

    added IntStrException

---
+++ IntStrException/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CustomException
+{
+    public class IntegerStException : Exception
+    {
+        public IntegerStException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class IntegerStringException
+    {
+
+
+        static void Main(string[] args)
+        {
+            IntegerStringException i = new IntegerStringException();
+            Console.WriteLine("Enter a Number : ");
+            try
+            {
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("The number is : " + n);
+            }
+            catch (Exception e)
+            {
+                Console.Write("It is in String Format!" + e.Message);
+            }
+        }
+    }
+}
